0642b2fede0c320498cad263ab96c5246890e9d8
Juan Felipe Hernandez Care <fcare8047@gmail.com>
2024-11-21 14:50:13 -0500

    cambio1

--- Simulador_Viajes/src/App.java
+++ Simulador_Viajes/src/App.java
@@ -1,4 +1,5 @@
-
+import java.util.Scanner;
+import java.util.Random;
 
 public class App {
 
@@ -6,15 +7,19 @@ public class App {
     static Scanner consola = new Scanner(System.in);
     static String[] planet = {"Mercurio", "Venus", "Marte", "Júpiter", "Saturno", "Urano", "Neptuno"};
     static double[] distance = {91.7, 42.4, 78.3, 628.9,  1284.4, 2721.4, 4345.4};
-    static String[] naves = {"Nave Leviatan", "Nave Olimpus", "Nave Pandora"};
+    static String[] naves = {"Orion", "Voyager", "Vostok", "Pandora", "Skywal"};
     static double[] speed = {900000.0, 100000.0, 50000.0};
+    static Random rm = new Random();
+    static int porcentaje;
+    static String[] events = {"Lluvia de Asteroides", "Daños en la Nave", "Desvio de la Nave", "Falta de Recursos"};
+    static int indiceAleatorio;
 
     public static void main(String[] args) throws Exception {
         Menu();
     }
 
     // Metodos
-    public static void Menu() {
+    public static void Menu() throws InterruptedException {
 
         int opc = 0;
 
@@ -40,21 +45,14 @@ public class App {
 
                 case 2:
                     // Metodo
-                    System.out.println("Mercurio bienvenido\n");
+                    SelectNave();
                     break;
 
                 case 3:
                     // Metodo
-                    System.out.println("   .   -- The Travel 12% ... --\r\n" + 
-                                       "  .'.\r\n" + 
-                                       "  |o|\r\n" + 
-                                       " .'o'.\r\n" + 
-                                       " |.-.|\r\n" + 
-                                       " '   '\r\n" + 
-                                       "  ( )\r\n" + 
-                                       "   )\r\n" + 
-                                       "  ( )\r\n");
-                    break;
+                        startTrip();
+
+                    break;    
 
                 case 4:
                     System.out.println("Saliendo del programa...");
@@ -70,7 +68,7 @@ public class App {
         consola.close();
     }
 
-    public static void SelectPlanet() {
+    public static void SelectPlanet() throws InterruptedException {
         int opcion = 0;
 
         do {
@@ -104,20 +102,119 @@ public class App {
 
             } else {
                 System.out.printf("\n** Su destino es el planeta (%s) con una distancia de (%s) Millones de Kilometros **\n", planet[opcion-1], distance[opcion-1]);
-            }
-
+            } SelectNave();
+            opcion = 9;
         } while (opcion!=9);
 
     }
 
-    public static void SelectNave() {
+    public static void SelectNave() throws InterruptedException {
+        int opcion = 0;
 
+        do {
+            System.out.println("         ,\r\n" + //
+                               "       _=|_\r\n" + //
+                               "     _[_## ]_\r\n" + //
+                               "_  +[_[_+_]P/    _    |_       ____      _=-\r\n" + 
+                               " ~---\\_I_I_[=\\--~ ~~--[o]--==-|##==]-=-~~ \r\n" + 
+                               "-~ /[_[_|_]_]\\\\  -_  [[=]]    |====]  __  \r\n" + 
+                               "  /    \"|\"    \\      ^U-U^  - |    - ~ .~\r\n" + 
+                               " ~~--__~~~--__~~-__   H_H_    |_     --     \r\n" + 
+                               "-. _  ~~~  ~~~=~~  -~~--~~  ~~~-    ~~--  ._\r\n" + 
+                               "             _     . -      _ _ ");
+             System.out.println("** SELECCIONE LA NAVE **");
+            System.out.println("╔═..═════════..═╗");
+            System.out.println("░ 01. Orion     ░");
+            System.out.println("░ 02. Voyager   ░");
+            System.out.println("░ 03. Vostok    ░");
+            System.out.println("░ 04. Pandora   ░");
+            System.out.println("░ 05. Skywal    ░");
+            System.out.println("░ 06. Salir..   ░");
+            System.out.println("╚═..═════════..═╝");
+            System.out.print("Digita una Opcion: ");
+            opcion = consola.nextInt();
+
+            if (opcion < 0 || opcion >= 6) {
+                System.out.println("Saliendo del Menú Naves...\n");
+                break;
+
+            } else {
+                System.out.printf("\n** Su nave es: (%s) **\n", naves[opcion-1]);
+            }
+            Resour();
+            opcion = 9;
+        } while (opcion!=9);
     }
 
-    public static void calculateResour() {
-
+    public static void Resour() throws InterruptedException {
+        System.out.println("Desea modificar los recusrsos a tu gusto? 0: Si. 1: No");
+        int calculo = consola.nextInt();
+       
+        if (calculo == 0) {
+            userResour();
+        } else {
+             startTrip();
+    
     }
 
 
 
 }
+
+    public static void userResour() throws InterruptedException {
+        System.out.println("Cuanto es la cantidad de oxigeno que desea agregar?");
+        double oxygen = consola.nextDouble();
+        System.out.println("Cuanto es la cantidad de combustible que desea agregar?");
+        double fuel = consola.nextDouble(); 
+        startTrip();
+
+    }
+
+    public static void startTrip() throws InterruptedException {
+
+        
+
+        int porcentaje;
+    
+        for (porcentaje = 1; porcentaje <= 100; porcentaje++) {
+            int mostrarEvento = rm.nextInt(10);
+    
+         if (mostrarEvento < 0.5) {
+            indiceAleatorio = rm.nextInt(events.length);
+            System.out.print("\\r  .   -- The Travel " + porcentaje + "% " + events[indiceAleatorio] + "... --" + 
+         "  .'.\r\n" + 
+         "  |o|\r\n" + 
+         " .'o'.\r\n" + 
+         " |.-.|\r\n" + 
+         " '   '\r\n" + 
+         "  ( )\r\n" + 
+         "   )\r\n" + 
+         "  ( )\r\n");
+        Thread.sleep(200);
+         stopSpaceShip();
+         System.out.println();
+         } else 
+         System.out.println("   .   -- The Travel " + porcentaje + "%" +  "... --\r\n" + 
+         "  .'.\r\n" + 
+         "  |o|\r\n" + 
+         " .'o'.\r\n" + 
+         " |.-.|\r\n" + 
+         " '   '\r\n" + 
+         "  ( )\r\n" + 
+         "   )\r\n" + 
+         "  ( )\r\n");
+         Thread.sleep(200);
+        System.out.println();
+       }
+        System.out.println("Viaje Finalizado con Exito");
+
+    
+    }
+
+    public static void stopSpaceShip() {
+        
+    }
+
+}
+
+
