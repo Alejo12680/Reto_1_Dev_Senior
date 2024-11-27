281899fbe66b7b05a2beed9a513d6840577b0496
Juan Felipe Hernandez Care <fcare8047@gmail.com>
2024-11-26 22:12:39 -0500

    Eliminar_Variables

--- Simulador_Viajes/src/App.java
+++ Simulador_Viajes/src/App.java
@@ -278,20 +278,10 @@ public class App {
 
     }
 
-<<<<<<< HEAD
-    public static void startTrip() throws InterruptedException {
+    
 
-         // Calcula para la cantidad necesaria para llegar al destino
-         var totalOxigen = cantPerson * days * oxigenPorDayPerson;
-         System.out.println(totalOxigen);
-
-         // Calculo necesario para el combustible alcance y llegue a su destino
-         var totalFuel = selectedDistence * fuelConsumptionPorKm;
-         System.out.println(totalFuel);
-
-=======
+        
     public static void StartTrip() throws InterruptedException {
->>>>>>> 92234670740bb56cd388f9d23589c4e570d3060c
         int porcentaje;
     
         for (porcentaje = 1; porcentaje <= 100; porcentaje++) {
